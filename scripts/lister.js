027de0d55f372d2f8e9b9870266b8786b4ac3b0d
Carlos Santiago <carlosdanielsantiagom@gmail.com>
2022-10-29 20:31:51 -0400

    Remove logic to only accept the same list item once, add some styles, add remove functionality

--- scripts/lister.js
+++ scripts/lister.js
@@ -1,31 +1,27 @@
-
 const addListItemBtn = document.querySelector(".addListItemBtn");
 
 const listElement = document.querySelector(".list");
 
 const inputForm = document.querySelector("#input-text");
 
-//This array will store all the <li><li/> items as they are created.
-const listItems = [];
-
-//This array will contain only unique values
-let uniqueListItems = [];
-
 const addItem = () => {
-  //Assign the non repeating values to the array based on the contents of the other array.
-  uniqueListItems = [...new Set(listItems)];
-
-  //create an new empty <li><li/> element every time the button is clicked
   let listItem = document.createElement("li");
+  let deleteBtn = document.createElement("button");
 
-  //Add text inside the new <li><li/>, the text is equal to the value of the input set to lower case.
-  listItem.innerText = inputForm.value.toLowerCase().trim();
+  deleteBtn.textContent = "-";
+  deleteBtn.setAttribute("type", "button");
+  deleteBtn.classList.add("deleteBtn");
 
-  //Add the text inside the <li><li/>  to the array for storage and comparison later
-  listItems.push(listItem.innerText);
+  const deleteItem = () => {
+    deleteBtn.parentElement.remove();
+  };
 
-  if (uniqueListItems.indexOf(listItem.innerText) === -1) {
+  deleteBtn.addEventListener("click", deleteItem);
+
+  listItem.innerText = inputForm.value.trim();
+  if (listItem.innerText !== "") {
     listElement.appendChild(listItem);
+    listItem.appendChild(deleteBtn);
   }
 };
 
